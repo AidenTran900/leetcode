58fe49add3498c1ae324f19cefff9af0a90001a5
Aiden Tran <54821554+AidenTran900@users.noreply.github.com>
2026-08-16 14:50:44 -0700

    Time: 55 ms (74.10%) | Memory: 22.1 MB (85.67%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -8,10 +8,10 @@ class Solution:
         for right in range(len(nums)):
             if nums[right] == 0:
                 zeroes += 1
-                while zeroes > k: # keep movin left until zeroes is <= k
-                    if nums[left] == 0:
-                        zeroes -= 1
-                    left += 1
+            while zeroes > k: # keep movin left until zeroes is <= k
+                if nums[left] == 0:
+                    zeroes -= 1
+                left += 1
 
             largest = max(largest, right - left + 1)
         
